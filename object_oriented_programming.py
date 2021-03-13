dc26a2b37837c6e77137137c75b91531036723e2
u-vm <u-vm@u-vm.localdomain>
2021-03-13 15:10:34 -0500

    Added super class Animal methods to Cat class

--- object_oriented_programming.py
+++ object_oriented_programming.py
@@ -1,12 +1,23 @@
 # Classes - A framework to describe an Object
 
+class Animal:
+    def __init__(self, number_eyes):
+        self.number_eyes = number_eyes
+
+    def breathe(self):
+            print("Inhale, exhale")
+
+    def eat(self):
+            print("I'm eating")
+
 # Instantiating a class:
-class Cat:
+class Cat(Animal): # Cat inherits from the super class Animal
 
     # Add a variable that is available to anything in this Class
     has_soul = False
 
     def __init__(self, name, age, color):
+        super().__init__(2)
         self.name = name
         self.age = age
         self.color = color
@@ -24,4 +35,14 @@ rupert.speak()
 lumpy = Cat('Lumpy', 12, 'black')
 
 lumpy.speak()
+
+# Inheritance (see line 3)
+
+cyclops_goat = Animal(1)
+
+print(rupert.number_eyes)
+
+kitten = Cat('Potato', 0, 'orange')
+
+print(kitten.number_eyes)
 
